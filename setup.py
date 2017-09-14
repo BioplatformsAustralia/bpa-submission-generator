6dbf226ee5d5d9f957e2dec20a218bdf95fe42bb
Grahame Bowland <grahame@angrygoats.net>
2017-09-14 16:17:09 +0800

    1.0.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(author="CCG, Murdoch University",
       keywords="",
       url="https://github.com/muccg/bpa-submission-generator",
       name="bpasubmit",
-      version="0.1.0",
+      version="1.0.0",
       packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
       entry_points={
           'console_scripts': [
